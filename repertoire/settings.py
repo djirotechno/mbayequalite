c7a9dc03e8066f35be84a099e1d3b6fc9d51a649
djirotechno <djirotech@gmail.com>
2024-05-24 16:00:30 +0000

    render conf5

--- repertoire/settings.py
+++ repertoire/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # ]
 
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
